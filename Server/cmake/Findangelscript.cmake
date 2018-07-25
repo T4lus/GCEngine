0e5d84f6063fa609c241c334cfaefebc19e3ab0c
T4lus <jeremy.rigaud@dust-of-world.com>
2018-07-25 15:15:52 +0200

    Cmake Test

--- Server/cmake/Findangelscript.cmake
+++ Server/cmake/Findangelscript.cmake
@@ -1,10 +1,16 @@
-find_path( angelscript_INCLUDE angelscript.h HINTS "${PROJECT_SOURCE_DIR}/dependency/AngelScript/angelscript/include" )
-find_library( angelscript_LIBRARY_STATIC libangelscript.a angelscript.lib HINTS  "${PROJECT_SOURCE_DIR}/dependency/AngelScript/angelscript/projects/cmake" )
 
-if ( angelscript_INCLUDE AND angelscript_LIBRARY_STATIC )
-    set( ANGELSCRIPT_FOUND TRUE )
-    message( STATUS "Found AngelScript include at: ${angelscript_INCLUDE}" )
-    message( STATUS "Found AngelScript library at: ${angelscript_LIBRARY_STATIC}" )
-else ( )
-    message( FATAL_ERROR "Failed to locate AngelScript dependency." )
-endif ( )
+find_path(angelscript_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/dependency/Angelscript")
+
+if((NOT angelscript_INCLUDE_DIR) OR (NOT EXISTS ${angelscript_INCLUDE_DIR})
+
+    message("Unable to locate AngelScript dependency.")
+
+    execute_process(COMMAND git submodule update --init -- dependency/Angelscript WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    set(angelscript_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dependency/Angelscript/angelscript/include")
+
+    install(DIRECTORY ${angelscript_INCLUDE_DIR}/include DESTINATION "${PROJECT_SOURCE_DIR}/Includes")
+else()
+    # see above, setup target as well
+endif()
+
+
